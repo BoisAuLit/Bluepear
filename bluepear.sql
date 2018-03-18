949534dcbf83a04c302ba487633d076b1cfcf0c0
omarlouriki93 <lourikiomar@hotmail.fr>
2018-03-18 14:22:17 +0100

    Add files via upload

--- bluepear.sql
+++ bluepear.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Jeu 08 Février 2018 à 19:54
+-- Généré le :  Dim 18 Mars 2018 à 13:18
 -- Version du serveur :  5.7.9
 -- Version de PHP :  5.6.16
 
@@ -23,14 +23,36 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `admin`
+--
+
+DROP TABLE IF EXISTS `admin`;
+CREATE TABLE IF NOT EXISTS `admin` (
+  `id_admin` int(11) NOT NULL AUTO_INCREMENT,
+  `password` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  PRIMARY KEY (`id_admin`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `admin`
+--
+
+INSERT INTO `admin` (`id_admin`, `password`, `nom`) VALUES
+(1, '123', 'bob'),
+(2, '1234', 'lili');
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `client`
 --
 
 DROP TABLE IF EXISTS `client`;
 CREATE TABLE IF NOT EXISTS `client` (
   `id_client` int(11) NOT NULL AUTO_INCREMENT,
-  `time_enter` datetime NOT NULL,
-  `time_exit` datetime NOT NULL,
+  `time_enter` datetime DEFAULT NULL,
+  `time_exit` datetime DEFAULT NULL,
   `list_positions` int(11) NOT NULL,
   PRIMARY KEY (`id_client`)
 ) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
@@ -52,10 +74,9 @@ INSERT INTO `client` (`id_client`, `time_enter`, `time_exit`, `list_positions`) 
 DROP TABLE IF EXISTS `position`;
 CREATE TABLE IF NOT EXISTS `position` (
   `id_pos` int(11) NOT NULL AUTO_INCREMENT,
-  `x` float NOT NULL,
-  `y` float NOT NULL,
-  `time_x` datetime NOT NULL,
-  `time_y` datetime NOT NULL,
+  `x` float DEFAULT NULL,
+  `y` float DEFAULT NULL,
+  `time_pos` datetime DEFAULT NULL,
   `id_client` int(11) NOT NULL,
   PRIMARY KEY (`id_pos`),
   UNIQUE KEY `index_client` (`id_client`)
@@ -65,9 +86,9 @@ CREATE TABLE IF NOT EXISTS `position` (
 -- Contenu de la table `position`
 --
 
-INSERT INTO `position` (`id_pos`, `x`, `y`, `time_x`, `time_y`, `id_client`) VALUES
-(2, 2.6, 1.3, '2018-02-05 04:24:23', '2018-02-05 17:33:49', 3),
-(3, 3.7, 5.4, '2018-02-08 11:31:28', '2018-02-08 14:32:29', 4);
+INSERT INTO `position` (`id_pos`, `x`, `y`, `time_pos`, `id_client`) VALUES
+(2, 2.6, 1.3, '2018-02-05 04:24:23', 3),
+(3, 3.7, 5.4, '2018-02-08 11:31:28', 4);
 
 --
 -- Contraintes pour les tables exportées
